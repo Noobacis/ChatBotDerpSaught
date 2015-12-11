19f6ee3b1e61d3240ff265644eb7ea51950d7f4e
Noobacis <noobacis.o@gmail.com>
2015-12-10 18:12:27 -0700

    meh

---
+++ ChatBot/ChatBotRunner.java
@@ -0,0 +1,7 @@
+package chatBotSourcePackage;
+
+public class ChatBotRunner {
+	public static void main(String[] args) {
+		System.out.println("meh");
+	}
+}
---
+++ ChatBot/src/chatBotSourcePackage/ChatBotRunner.java
@@ -0,0 +1,5 @@
+package chatBotSourcePackage;
+
+public class ChatBotRunner {
+
+}
